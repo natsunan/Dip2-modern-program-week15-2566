fc84c7877d16bbb1ea4e6dcacbf0ca447438e726
Natsunsn <Natsunan270@gmail.com>
2024-01-22 16:16:05 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/futter_week14/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.futter_week14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/list_view_menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListViewMenu extends StatelessWidget {
+  const ListViewMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      appBar: AppBar(),
+      body: container(),
+    );
+  }
+}
